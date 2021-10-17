4ae28a742d45ccd84c5edb11b8d00d60991b4b17
Jessica Mauerhan <jessicamauerhan@gmail.com>
2021-10-16 19:03:05 -0500

    Update round-4.php

--- round-4.php
+++ round-4.php
@@ -43,7 +43,7 @@
         ],
         [
             'name' => 'CONSTANTS',
-            'questions' => 
+            'questions' => [
                 ['question' => "This constant's value represents the largest integer supported in the running build of PHP", 'answer'=>'PHP_INT_MAX'],
                 ['question' => "This error-reporting constant represents run-time notices about code that will not work in future versions", 'answer'=>'E_DEPRECATED'],
                 ['question' => "This platform-dependent constant tells you where one line ends and another begins", 'answer'=>'PHP_EOL'],
